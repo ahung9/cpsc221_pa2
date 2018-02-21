0487fbb6b83ca30f770d4298affd32d182c5bf8c
AbidSalahi <abidsalahi1377@gmail.com>
2018-02-21 15:01:23 -0800

    abid's initial

---
+++ stack.cpp
@@ -0,0 +1,40 @@
+#include "stack.h"
+
+
+// returns the top element of the stack
+// returns a default (0,0) point if stack is empty
+Point Stack::peek(){
+    if (st.size() != 0){
+        return st[st.size() - 1];
+    } else {
+        Point p;
+         return p;
+    }
+}
+
+// returns number of elements in the stack
+int Stack::size(){
+    st.size();
+}
+
+// returns true if the stack is empty, false otherwise
+bool Stack::isEmpty(){
+    return st.size() == 0;
+}
+
+
+// mutators
+// adds an item to the top of the stack
+void Stack::push(Point p){
+    st.push_back(p);
+}
+
+// removes and returns the top element of the stack
+// returns a default (0,0) point if stack is empty
+Point pop(){
+    Point p;
+    return p;
+}
+
+
+
