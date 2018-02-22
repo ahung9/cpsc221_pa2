181644a81e55983a529166970a65a7d2dde6b2aa
AbidSalahi <abidsalahi1377@gmail.com>
2018-02-21 17:37:37 -0800

    editted convexHull

--- convexhull.cpp
+++ convexhull.cpp
@@ -11,47 +11,43 @@
 // 	one of the legs of the angle is represened by the line through v[0] and
 //	v[i], and the other is the x-axis.
 void sortByAngle(vector<Point>& v){
-  findLowestY(v);
-  Point lyp = v[0];
-  vector<double> slopes = slopes(v);
-
-  for (int i = 0; int < (int)v.size() - 1; i++){
-    int index = i;
-    int min = slopes[i];
-
-    //find index of min from i-j
-    for(int j = i; j < (int)v.size(); j++){
-      if(slopes[j] < min){
-        min = slopes[j];
-        index = j;
-      }
+    
+    findLowestY(v);
+   
+    vector<double> slopesVector;
+    slopesVector = slopes(v);
+    
+    for(int i = 0; i < (int) v.size() - 1; ++i){
+        
+        int smallest = i;
+        
+        
+        for (int j = i + 1;  j < (int)v.size(); ++j){
+            
+            if (slopesVector[j] < slopesVector[smallest]){
+                smallest = j;
+            }
+        }
+        
+        // swap with smallest
+        Point temp = v[i];
+        v[i] = v[smallest];
+        v[smallest] = temp;
+        
+        // swap elements in slopes as well to match points vector
+        double slopeTemp = slopesVector[i];
+        slopesVector[i] = slopesVector[smallest];
+        slopesVector[smallest] = slopeTemp;
     }
-    double temp_slope = slopes[i];
-    slopes[i] = slopes[index];
-    slopes[index] = temp_slope;
-
-    Point temp = v[i];
-    v[i] = v[index];
-    v[index] = temp;
-  }
-}
-
-vector<double> slopes (vector<Point>){
-  vector <double> slopes;
-  slopes.push_back(0);
-  for(int i = 1; i < (int)v.size(); i++)+{
-    slopes.push_back(slope(v[0],v[i]));
-    return slopes;
-  }
-}
-
-double slope(Point p1, Point p2){
-    return abs((p2.y - p1.y) / (p2.x - p1.x));
+    
+    
+    return;
 }
 
 // determines whether a path from p1 to p2 to p3 describes a counterclockwise turn
 bool ccw(Point p1, Point p2, Point p3){
-    return false;
+    return ((p2.x - p1.x)*(p3.y - p1.y) - (p2.y - p1.y)*(p3.x - p1.x)) >= 0;
+        
 }
 
 // returns a vector of points representing the convex hull of v
@@ -61,33 +57,52 @@ bool ccw(Point p1, Point p2, Point p3){
 // The returned vector should be a subset of v
 // Input: v - a vector of points in arbitrary order
 vector<Point> getConvexHull(vector<Point>& v){
-    vector<Point> points;
-    return points;
+    sortByAngle(v);
+    //vector<Point> points;
+    for (int i = 1; i < (int) v.size() - 1; i++){
+        if (!ccw(v[i-1], v[i], v[i+1])){
+             v.erase (v.begin()+ (i - 1));
+        }
+    }
+        
+    return v;
 }
 
 // You may add declarations for some of your own useful functions here,
 // BUT DO NOT MODIFY ANY OF THE FUNCTIONS ABOVE
 
-
-
+double slope(Point p1, Point p2){
+    return abs((p2.y - p1.y) / (p2.x - p1.x));
+}
 
 void findLowestY(vector<Point> &v){
     Point ret = v[0];
     int index = 0;
-
-    for (int i = 1; i < (int)v.size(); i++){
+    for (int i = 1; i < (int) v.size(); i++){
         if (v[i].y < ret.y){
             ret = v[i];
             index = i;
         }
-
+        
         if (v[i].y == ret.y && v[i].x < ret.x){
             ret = v[i];
-            index = i;
         }
     }
-
+    
     Point temp = v[0];
     v[0] = ret;
     v[index] = temp;
 }
+
+vector<double> slopes(vector<Point> v){
+    vector<double> slopes;
+    slopes.push_back(0);
+    for (int i = 1; i < (int) v.size(); i++){
+        slopes.push_back(slope(v[0], v[i]));
+    }
+    
+    return slopes;
+}
+
+
+
